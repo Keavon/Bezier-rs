962c628ff7da08d6b2b8df30a8a20a6c16b6c984
0HyperCube <78500760+0HyperCube@users.noreply.github.com>
2024-02-04 06:05:26 +0000

    Bezier-rs: Use nonzero winding order for Poisson-disk insideness test (#1590)
    * Proper winding order for poisson dist
    
    * More robust cubic solving
    
    * Fix test expecting roots in a different order
    
    * Manual sort impl
    
    * Code review nits
    
    ---------
    
    Co-authored-by: Keavon Chambers <keavon@keavon.com>

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,3 @@ mod utils;
 pub use bezier::*;
 pub use subpath::*;
 pub use utils::{Cap, Join, SubpathTValue, TValue, TValueType};
-
-// TODO: Remove this, it is kept for semver backwards compatibility but never should have been public to begin with.
-#[deprecated]
-#[derive(Debug, Clone)]
-pub struct Bezier1d(pub Vec<f64>);
