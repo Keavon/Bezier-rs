51590641ffac122e75ea726144cbc93e059a1760
jneem <joeneeman@gmail.com>
2025-08-13 14:10:10 -0500

    Add poly-cool as a root finder (#3031)
    * Convert tangents_to_point
    
    * Convert normals_to_point
    
    * Port over roots and project
    
    * Remove symmetrical_basis
    
    * Code style nits
    
    ---------
    
    Co-authored-by: Keavon Chambers <keavon@keavon.com>

---
+++ src/bezier/poly_cool/poly.rs
@@ -0,0 +1,231 @@
+// This file is inlined from https://github.com/jneem/poly-cool with permission of its author, Joe Neeman.
+
+//! Polynomials of dynamic (run-time) degree.
+
+use super::{different_signs, Cubic, InputError, TerminationCondition};
+
+/// A polynomial of dynamic degree.
+///
+/// It would be nice to have polynomials of type-level degree,
+/// but that's a bit awkward without const generic expressions
+/// (e.g. to express the type of the derivative). It could be
+/// done with `typenum` and `generic_array`...
+#[derive(Clone, Debug)]
+pub struct Poly {
+    /// Coefficients in increasing order of degree.
+    ///
+    /// For example, `coeffs[0]` is the constant term.
+    coeffs: Vec<f64>,
+}
+
+impl<'a> std::ops::Mul<&'a Poly> for &'a Poly {
+    type Output = Poly;
+
+    fn mul(self, rhs: &Poly) -> Poly {
+        let mut coeffs = vec![0.0; (self.coeffs.len() + rhs.coeffs.len()).saturating_sub(1)];
+
+        for (i, c) in self.coeffs.iter().enumerate() {
+            for (j, d) in rhs.coeffs.iter().enumerate() {
+                coeffs[i + j] += c * d;
+            }
+        }
+        Poly { coeffs }
+    }
+}
+
+impl std::ops::Mul<&Poly> for Poly {
+    type Output = Poly;
+
+    fn mul(self, rhs: &Poly) -> Poly {
+        (&self) * rhs
+    }
+}
+
+impl Poly {
+    /// Constructs a new polynomial from coefficients.
+    ///
+    /// The first coefficient provided will be the constant term, the second will
+    /// be the linear term, and so on.
+    pub fn new(coeffs: impl IntoIterator<Item = f64>) -> Self {
+        Poly {
+            coeffs: coeffs.into_iter().collect(),
+        }
+    }
+
+    fn is_finite(&self) -> bool {
+        self.coeffs.iter().all(|c| c.is_finite())
+    }
+
+    /// Returns the polynomial that's the derivative of this polynomial.
+    pub fn deriv(&self) -> Poly {
+        let mut coeffs = Vec::with_capacity(self.coeffs.len() - 1);
+        // If we're empty (meaning that we're the constant zero polynomial),
+        // this will just return the zero polynomial again: no need for a
+        // special case.
+        for (i, c) in self.coeffs.iter().enumerate().skip(1) {
+            coeffs.push(c * (i as f64));
+        }
+        Poly { coeffs }
+    }
+
+    /// Evaluates this polynomial at a point.
+    pub fn eval(&self, x: f64) -> f64 {
+        let mut ret = 0.0;
+        let mut x_pow = 1.0;
+        for &c in &self.coeffs {
+            ret += c * x_pow;
+            x_pow *= x;
+        }
+        ret
+    }
+
+    /// The degree of this polynomial.
+    ///
+    /// This function only looks at the *presence* of coefficients, not their
+    /// value. If you construct a polynomial with three coefficients, this
+    /// method will say that it has degree 2 even if all of those coefficients
+    /// are zero.
+    ///
+    /// A polynomial with no coefficients will give zero as its degree, as will
+    /// a polynomial with one coefficient.
+    pub fn degree(&self) -> usize {
+        self.coeffs.len().saturating_sub(1)
+    }
+
+    /// If this polynomial has degree 3 or less, converts it to a [cubic](crate::Cubic).
+    fn to_cubic(&self) -> Option<Cubic> {
+        if self.degree() <= 3 {
+            Some(Cubic {
+                c0: self.coeffs.first().copied().unwrap_or(0.0),
+                c1: self.coeffs.get(1).copied().unwrap_or(0.0),
+                c2: self.coeffs.get(2).copied().unwrap_or(0.0),
+                c3: self.coeffs.get(3).copied().unwrap_or(0.0),
+            })
+        } else {
+            None
+        }
+    }
+
+    fn one_root<Term: TerminationCondition>(
+        &self,
+        deriv: &Poly,
+        mut lower: f64,
+        mut upper: f64,
+        val_lower: f64,
+        val_upper: f64,
+        term: Term,
+    ) -> f64 {
+        if !val_lower.is_finite() || !val_upper.is_finite() || !deriv.is_finite() {
+            return f64::NAN;
+        }
+        debug_assert!(different_signs(val_lower, val_upper));
+
+        let mut x = lower + (upper - lower) / 2.0;
+        let mut val_x = self.eval(x);
+        let mut step = (upper - lower) / 2.0;
+
+        while x.is_finite() && !term.stop(step, val_x) {
+            let root_in_first_half = different_signs(val_lower, val_x);
+            if root_in_first_half {
+                upper = x;
+            } else {
+                lower = x;
+            }
+
+            let deriv_x = self.deriv().eval(x);
+            debug_assert!(deriv_x.is_finite());
+            debug_assert!(val_x.is_finite());
+
+            step = -val_x / deriv_x;
+            let mut new_x = x + step;
+
+            if new_x <= lower || new_x >= upper {
+                new_x = lower + (upper - lower) / 2.0;
+
+                if new_x == upper || new_x == lower {
+                    // This should be rare, but it happens if they ask for more
+                    // accuracy than is reasonable. For example, suppse (because
+                    // of large coefficients) the output value jumps from -1.0
+                    // to 1.0 between adjacent floats and they ask for an output
+                    // error of smaller than 0.5. Then we'll eventually shrink
+                    // the search interval to a pair of adjacent floats and hit
+                    // this case.
+                    return new_x;
+                }
+            }
+            step = new_x - x;
+            x = new_x;
+            val_x = self.eval(x);
+        }
+        x
+    }
+
+    /// Finds all the roots in an interval, using Yuksel's algorithm.
+    ///
+    /// This is a numerical, iterative method. It first constructs critical
+    /// points to find bracketing intervals (intervals `[x0, x1]` where
+    /// `self.eval(x0)` and `self.eval(x1)` have different signs). Then it uses
+    /// a kind of modified Newton method to find a root on each bracketing
+    /// interval. It has a few limitations:
+    ///
+    /// - if there is only a small interval where the polynomial changes sign,
+    ///   it can miss roots. For example, when two roots are very close together
+    ///   it can miss them both.
+    /// - run time is quadratic in the degree. However, it is often very fast
+    ///   in practice for polynomials of low degree, especially if the interval
+    ///   `[lower, upper]` contains few roots.
+    pub fn roots_between(&self, lower: f64, upper: f64, x_error: f64) -> Vec<f64> {
+        let mut ret = Vec::new();
+        let mut scratch = Vec::new();
+        self.roots_between_with_buffer(lower, upper, x_error, &mut ret, &mut scratch);
+        ret
+    }
+
+    /// Finds all the roots in an interval, using Yuksel's algorithm.
+    ///
+    /// See [`Poly::roots_between`] for more details. This method differs from that
+    /// one in that it performs fewer allocations: you provide an `out` buffer
+    /// for the result and a `scratch` buffer for intermediate computations.
+    pub fn roots_between_with_buffer(
+        &self,
+        lower: f64,
+        upper: f64,
+        x_error: f64,
+        out: &mut Vec<f64>,
+        scratch: &mut Vec<f64>,
+    ) {
+        out.clear();
+        scratch.clear();
+
+        if let Some(c) = self.to_cubic() {
+            out.extend(c.roots_between(lower, upper, x_error));
+            return;
+        }
+
+        let deriv = self.deriv();
+        deriv.roots_between_with_buffer(lower, upper, x_error, scratch, out);
+        scratch.push(upper);
+        out.clear();
+        let mut last = lower;
+        let mut last_val = self.eval(last);
+
+        // `scratch` now contains all the critical points (in increasing order)
+        // and the upper endpoint of the interval. If we throw away all the
+        // critical points that are outside of (lower, upper), the things
+        // remaining in `scratch` are the endpoints of the potential bracketing
+        // intervals of our polynomial. So by filtering out uninteresting
+        // critical points, this loop is iterating over potential bracketing
+        // intervals.
+        for &mut x in scratch {
+            if x > last && x <= upper {
+                let val = self.eval(x);
+                if different_signs(last_val, val) {
+                    out.push(self.one_root(&deriv, last, x, last_val, val, InputError(x_error)));
+                }
+
+                last = x;
+                last_val = val;
+            }
+        }
+    }
+}
---
+++ src/bezier/poly_cool/quadratic.rs
@@ -0,0 +1,154 @@
+// This file is inlined from https://github.com/jneem/poly-cool with permission of its author, Joe Neeman.
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Quadratic {
+    pub c0: f64,
+    pub c1: f64,
+    pub c2: f64,
+}
+
+impl std::ops::Mul<f64> for Quadratic {
+    type Output = Quadratic;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self {
+            c0: self.c0 * rhs,
+            c1: self.c1 * rhs,
+            c2: self.c2 * rhs,
+        }
+    }
+}
+
+impl std::ops::Div<f64> for Quadratic {
+    type Output = Quadratic;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        Self {
+            c0: self.c0 / rhs,
+            c1: self.c1 / rhs,
+            c2: self.c2 / rhs,
+        }
+    }
+}
+
+impl Quadratic {
+    pub fn eval(&self, x: f64) -> f64 {
+        self.c0 + self.c1 * x + self.c2 * x * x
+    }
+
+    pub fn is_finite(&self) -> bool {
+        self.c0.is_finite() && self.c1.is_finite() && self.c2.is_finite()
+    }
+
+    pub fn roots(&self) -> Vec<f64> {
+        let a = self.c2;
+        let b = self.c1;
+        let c = self.c0;
+        let disc = b * b - 4.0 * a * c;
+        if disc.is_finite() {
+            let mut ret = Vec::new();
+            let mut push = |r: f64| {
+                if r.is_finite() {
+                    ret.push(r)
+                }
+            };
+            if disc > 0.0 {
+                let q = -0.5 * (b + disc.sqrt().copysign(b));
+                let r0 = q / a;
+                let r1 = c / q;
+                push(r0.min(r1));
+                push(r0.max(r1));
+            } else if disc == 0.0 {
+                let root = -0.5 * b / a;
+                if root.is_finite() {
+                    push(root);
+                } else if c == 0.0 {
+                    // This is kurbo's behavior: the intention is that if the
+                    // whole thing is zero, return zero as a single root. I'm
+                    // not sure I love it.
+                    //
+                    // Bear in mind that this branch is not *only* for the
+                    // identically zero case: if a == c == 0.0 and b * b
+                    // underflows then we will end up here. In that case,
+                    // zero is the only root.
+                    push(0.0);
+                }
+            } else {
+                // No roots.
+            }
+            ret
+        } else {
+            // At least one of the coefficients was too large and triggered
+            // overflow.
+            //
+            // The exponent of f64 maxes out at 1023, so scaling down by
+            // 2^{-512} is enough to ensure that squaring doesn't overflow. We
+            // do an extra factor of 2^{-3} for some wiggle room. This can't
+            // completely destroy all the coefficients: because of the overflow,
+            // we know that at least one of them was big.
+            let scale = 2.0f64.powi(-515);
+            // TODO: this can stack overflow if we're infinite. How should
+            // we handle that?
+            (*self * scale).roots()
+        }
+    }
+
+    pub fn positive_discriminant_roots(&self) -> Option<(f64, f64)> {
+        let a = self.c2;
+        let b = self.c1;
+        let c = self.c0;
+        let disc = b * b - 4.0 * a * c;
+        if disc.is_finite() {
+            if disc > 0.0 {
+                let q = -0.5 * (b + disc.sqrt().copysign(b));
+                let r0 = q / a;
+                let r1 = c / q;
+                Some((r0.min(r1), r0.max(r1)))
+            } else {
+                None
+            }
+        } else {
+            self.positive_discriminant_roots_scaled()
+        }
+    }
+
+    #[cold]
+    fn positive_discriminant_roots_scaled(&self) -> Option<(f64, f64)> {
+        if self.is_finite() {
+            let scale = 2.0f64.powi(-515);
+            (*self * scale).positive_discriminant_roots()
+        } else {
+            None
+        }
+    }
+
+    pub fn positive_discriminant_roots_no_overflow_check(&self) -> Option<(f64, f64)> {
+        let a = self.c2;
+        let b = self.c1;
+        let c = self.c0;
+        let disc = b * b - 4.0 * a * c;
+        if disc > 0.0 {
+            let q = -0.5 * (b + disc.sqrt().copysign(b));
+            let r0 = q / a;
+            let r1 = c / q;
+            Some((r0.min(r1), r0.max(r1)))
+        } else {
+            None
+        }
+    }
+
+    pub fn positive_discriminant_roots_no_overflow_check_half_b(&self) -> Option<(f64, f64)> {
+        let a = self.c2;
+        let b = self.c1;
+        let c = self.c0;
+        let disc = b * b - a * c;
+        if disc > 0.0 {
+            let q = -(b + disc.sqrt().copysign(b));
+            let r0 = q / a;
+            let r1 = c / q;
+            Some((r0.min(r1), r0.max(r1)))
+        } else {
+            None
+        }
+    }
+}
